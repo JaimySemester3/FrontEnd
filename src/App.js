b19447fd4e9a9940b66d25c866146b6c5f240188
JaimyDerks <315190@student.fontys.nl>
2023-12-07 09:58:54 +0100

    Added Login functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import TaskList from "./components/TaskList.js";
+import Navbar from './components/NavBar.js';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <TaskList/>
     </div>
   );
